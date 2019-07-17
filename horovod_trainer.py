fee8ffd2261923b04dd7f0263e77839f9c8a5671
root <root@perf49.perf.lab.eng.bos.redhat.com>
2019-07-17 15:06:07 -0400

    Uppdate horovod trainer

--- horovod_trainer.py
+++ horovod_trainer.py
@@ -1,20 +1,15 @@
 import os
 import time
 import torch
-import torch.distributed as dist
 import torch.nn as nn
 import torch.optim as optim
-import torch.multiprocessing as mp
 import torch.optim as optim
 import torch.nn.functional as F
-from tqdm import tqdm
-from torch.utils.data import DataLoader
 from torch.utils.data.distributed import DistributedSampler
-import distributed.utils as d_utils
+from torch.utils.data import DataLoader
 from model import Lda2vec
 from loss.dirichlet import DirichletLoss
 from loss.sgns import SGNSLoss
-from torch.nn.parallel import DistributedDataParallel as DDP
 import horovod.torch as hvd
 
 class HorovodTrainer:
@@ -45,26 +40,28 @@ class HorovodTrainer:
         dirichlet = DirichletLoss()
 
         #Distributed optimizer
-        optimizer = optim.Adam(model.parameters(), lr=self.args.lr)
-        
+        optimizer = optim.Adam(model.parameters(), lr=self.args.lr * hvd.size())
+       
+       # Broadcast from rank 0 to all other processes
+        hvd.broadcast_parameters(model.state_dict(), root_rank=0)
+        hvd.broadcast_optimizer_state(optimizer, root_rank=0)
+
         if self.args.compression:
-            compression = hvd.Compression.fp16 if args.fp16_allreduce else hvd.Compression.none
+            compression = hvd.Compression.fp16
             optimizer = hvd.DistributedOptimizer(optimizer, named_parameters=model.named_parameters(), compression=compression)
             print('Using compression: {}'.format(compression))
         else:
             optimizer = hvd.DistributedOptimizer(optimizer, named_parameters=model.named_parameters())
 
-        # Broadcast from rank 0 to all other processes
-        hvd.broadcast_parameters(model.state_dict(), root_rank=0)
-
         # Log time
         begin_time = time.perf_counter()
         print("Began Training At:", begin_time)
+        print("Using {} GPUs".format(hvd.size()))
 
         global_step = 0
         for epoch in range(self.args.epochs):
             running_loss = 0.0
-            for i, data in tqdm(enumerate(dataloader)):
+            for i, data in enumerate(dataloader):
                 # unpack data
                 (center, doc_id), target = data
                 center, doc_id, target = center.cuda(), doc_id.cuda(), target.cuda()
@@ -85,11 +82,11 @@ class HorovodTrainer:
                 running_loss += loss
                 if global_step % self.args.log_step == 0:
                     norm = (i + 1) * self.args.batch_size
-                    print('EPOCH: {} | STEP: {} | LOSS {}'.format(
-                        epoch, global_step, running_loss/norm
+                    print('EPOCH: {} | STEP: {} | TIME: {} | LOSS {}'.format(
+                        epoch, global_step, time.perf_counter() - begin_time, running_loss/norm
                     ))
 
         # Log time
         end_time = time.perf_counter()
         print("Ended Training At:", end_time)
-        print("Training Lasted: {} ms".format(end_time - begin_time))
+        print("Training Lasted: {} s".format(end_time - begin_time))
