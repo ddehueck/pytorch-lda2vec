f3dfd9a77a02d25667fcdbeb566a0f9488b0d28e
d.dehueck@gmail.com <d.dehueck@gmail.com>
2019-08-06 14:12:24 -0400

    Use log softmax for numerical stability instead of epsilon

--- loss/dirichlet.py
+++ loss/dirichlet.py
@@ -3,14 +3,13 @@ import torch.nn.functional as F
 import torch.nn as nn
 
 class DirichletLoss(nn.Module):
-    EPSILON = 1e-5
-    
+
     def __init__(self, args):
         super(DirichletLoss, self).__init__()
-        self.alpha = 1 / args.num_topics
+        self.alpha = 1.0 / args.num_topics
         self.lambda_val = args.lambda_val
 
     def forward(self, doc_weights):
-        proportions = F.softmax(doc_weights, dim=0).clamp(min=self.EPSILON, max=1-self.EPSILON)
-        avg_log_proportion = t.sum(t.log(proportions), dim=2).mean()
+        log_proportions = F.log_softmax(doc_weights, dim=0)
+        avg_log_proportion = t.sum(log_proportions, dim=2).mean()
         return -self.lambda_val * (self.alpha - 1) * avg_log_proportion
